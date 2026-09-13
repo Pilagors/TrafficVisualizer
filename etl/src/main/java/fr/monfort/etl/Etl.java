d88572960223f4998277c25868a8f24984a8ceb1
Lucas Monfort <Lucas_Monfort@etu.ube.fr>
2026-09-13 15:23:42 +0200

    feat(back): ajout d'un projet maven etl pour l'insertion de données statiques
    - déclaration d'un pom parent

---
+++ etl/src/main/java/fr/monfort/etl/Etl.java
@@ -0,0 +1,7 @@
+package fr.monfort.etl;
+
+public class Etl {
+    public static void main(String[] args) {
+        System.out.println("Hello, Etl!");
+    }
+}
---
+++ etl/src/test/java/fr/monfort/etl/EtlTest.java
@@ -0,0 +1,16 @@
+package fr.monfort.etl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+class EtlTest {
+    @Test
+    void contextLoads() {
+
+        assertTrue(true);
+
+    }
+}
+
